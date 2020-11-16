37a9efe2b402091d5fccee0ea3bb59b6bfebadad
José Gomes <j.da.silva8.gomes@gmail.com>
2020-11-16 19:35:53 +0000

    changing homepage. adding instructions section. adding animation to logo transition to navbar. adding enter interaction for start. sobre not opening yet.

--- js/all.js
+++ js/all.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
   $endPosition = false;
   $cell = document.querySelectorAll(".cell-drag");
-  console.log($cell.length);
+  $logo = document.querySelectorAll(".logo");
+  $navbar = document.getElementById("navbar");
+  $logoSmaller = false;
 
   if ($cell.length > 0) {
     $(".cell-drag").draggable({
@@ -15,16 +17,16 @@ $(document).ready(() => {
 
   $(".cell-drag").on('drag', (event, ui) => {
     console.log(ui.position.top);
-    if(ui.position.top < 0) {
+    if (ui.position.top < 0) {
       $(".cell-drag").draggable('disabled');
     }
-    if(ui.position.top >= 250) {
+    if (ui.position.top >= 250) {
       $endPosition = true;
     } else {
       $endPosition = false;
     }
 
-    if($endPosition === true) {
+    if ($endPosition === true) {
       setTimeout(() => {
         document.querySelector('#interaction-two').scrollIntoView({
           behavior: 'smooth'
@@ -34,8 +36,42 @@ $(document).ready(() => {
   })
 
   $("#chevron-down").click(() => {
-    document.querySelector('#interaction-one').scrollIntoView({
+    if ($logo.length == 1) {
+      $logo[0].classList.add("adjust-position");
+      $logoSmaller = true;
+    }
+    document.querySelector('#instructions').scrollIntoView({
       behavior: 'smooth'
     });
   });
+});
+
+$(window).scroll(() => {
+  if ($logo.length == 1) {
+    if ($(this).scrollTop() > 400) {
+      $logo[0].classList.add("adjust-position");
+      $navbar.classList.add("adjust-size");
+      $logoSmaller = false;
+    }
+    if ($(this).scrollTop() < 400 && !$logoSmaller) {
+      $logo[0].classList.remove("adjust-position");
+      $navbar.classList.remove("adjust-size");
+    }
+  }
+})
+
+$.fn.isInViewport = function () {
+  let elementTop = $(this).offset().top;
+  let elementBottom = elementTop + $(this).outerHeight();
+
+  let viewportTop = $(window).scrollTop();
+  let viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(document).keydown((e) => {
+  if (e.which === 13 && $("#interaction-start").isInViewport()) {
+    console.log(e.which);
+  }
 });
